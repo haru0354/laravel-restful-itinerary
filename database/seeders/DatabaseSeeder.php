8d9ef3c07cf9b02af59ecfabf0835f12ca942bc5
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-20 12:12:48 +0900

    TripSeederの作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +18,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
+            TripSeeder::class,
+
         ]);
     }
 }
