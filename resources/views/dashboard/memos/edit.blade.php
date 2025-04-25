a67958e0632a1c7f9c02360664049a843ae47244
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-25 15:40:40 +0900

    memoのeditのviewの作成

---
+++ resources/views/dashboard/memos/edit.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $trip_title }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl w-full mx-auto p-6 bg-white shadow-sm">
+            <x-form.form-container
+                title="メモの編集" buttonTitle="編集する" backRoute="{{ route('dashboard.trips.memos.show', ['trip_id' => $memo->trip_id, 'memo_id' =>$memo->id]) }}">
+                <x-form.input name="title" value="{{ $memo->title }}">メモタイトル</x-form.input>
+                <x-form.input name="content" value="{{ $memo->content }}">メモの内容</x-form.input>
+            </x-form.form-container>
+        </div>
+    </div>
+</x-app-layout>
