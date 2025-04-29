5ad5f2937b3f4ecdba8e96f0dcdc5765b1ed80e4
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-29 15:45:34 +0900

    textareaコンポーネントにエラー時のメッセージの表示を追加

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -5,4 +5,7 @@
     <textarea id="{{ $name }}" name="{{ $name }}"
         {{ $attributes->merge(['class' => "w-full h-32 py-2 border rounded border-gray-300 bg-gray-50 focus:bg-white transition-colors duration-200 ease-in-out"]) }}
         >{{ old($name, $value) }}</textarea>
+    @error($name)
+    <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+    @enderror
 </div>
