4568d7b4b0e2ec9f5e711639ac99110d847abe5c
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-04-30 16:31:53 +0900

    memoの各種viewにsectionタグの追加

--- resources/views/dashboard/memos/create.blade.php
+++ resources/views/dashboard/memos/create.blade.php
@@ -7,11 +7,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl w-full mx-auto p-6 bg-white shadow-sm">
-            <x-form.form-container method="POST" action="{{ route('dashboard.trips.memos.store', ['trip_id' => $trip_id]) }}"
-                title="メモの登録" buttonTitle="新規登録する" backRoute="{{ route('dashboard.trips.memos.index', ['trip_id' => $trip_id]) }}">
-                <x-form.input name="title">メモのタイトル</x-form.input>
-                <x-form.textarea name="content">メモする内容</x-form.textarea>
-            </x-form.form-container>
+            <section>
+                <x-form.form-container method="POST" action="{{ route('dashboard.trips.memos.store', ['trip_id' => $trip_id]) }}"
+                    title="メモの登録" buttonTitle="新規登録する" backRoute="{{ route('dashboard.trips.memos.index', ['trip_id' => $trip_id]) }}">
+                    <x-form.input name="title">メモのタイトル</x-form.input>
+                    <x-form.textarea name="content">メモする内容</x-form.textarea>
+                </x-form.form-container>
+            </section>
         </div>
     </div>
 
--- resources/views/dashboard/memos/edit.blade.php
+++ resources/views/dashboard/memos/edit.blade.php
@@ -7,14 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl w-full mx-auto p-6 bg-white shadow-sm">
-            <x-form.form-container method="POST" action="{{ route('dashboard.trips.memos.update', ['trip_id' => $memo->trip_id, 'memo_id' =>$memo->id]) }}"
-                title="メモの編集" buttonTitle="編集する" backRoute="{{ route('dashboard.trips.memos.show', ['trip_id' => $memo->trip_id, 'memo_id' =>$memo->id]) }}">
-                @method('PUT')
-                <x-form.input name="title" value="{{ $memo->title }}">メモタイトル</x-form.input>
-                <x-form.textarea name="content" value="{{ $memo->content }}">メモの内容</x-form.textarea>
-            </x-form.form-container>
-            <x-ui.delete-modal title="{{ $memo->title }}" buttonTitle="削除する"
-                action="{{ route('dashboard.trips.memos.destroy', ['trip_id' => $memo->trip_id, 'memo_id' => $memo->id]) }}" />
+            <section>
+                <x-form.form-container method="POST" action="{{ route('dashboard.trips.memos.update', ['trip_id' => $memo->trip_id, 'memo_id' =>$memo->id]) }}"
+                    title="メモの編集" buttonTitle="編集する" backRoute="{{ route('dashboard.trips.memos.show', ['trip_id' => $memo->trip_id, 'memo_id' =>$memo->id]) }}">
+                    @method('PUT')
+                    <x-form.input name="title" value="{{ $memo->title }}">メモタイトル</x-form.input>
+                    <x-form.textarea name="content" value="{{ $memo->content }}">メモの内容</x-form.textarea>
+                </x-form.form-container>
+                <x-ui.delete-modal title="{{ $memo->title }}" buttonTitle="削除する"
+                    action="{{ route('dashboard.trips.memos.destroy', ['trip_id' => $memo->trip_id, 'memo_id' => $memo->id]) }}" />
+            </section>
         </div>
     </div>
 </x-app-layout>
