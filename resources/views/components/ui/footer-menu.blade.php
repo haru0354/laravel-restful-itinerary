e0dff79ee14da31a68a8bb7044252aca7c42250c
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-05-01 15:42:53 +0900

    footerNenuのデザインをスマホ風のデザインに変更

--- resources/views/components/ui/footer-menu.blade.php
+++ resources/views/components/ui/footer-menu.blade.php
@@ -4,23 +4,46 @@ $currentRoute = Route::currentRouteName();
 @endphp
 
 @if($tripId)
-<div class="footer-menu fixed bottom-0 w-full bg-white  border-gray-400 border-t">
-    <div class="flex justify-around py-2">
-        <a href="{{ route('dashboard.trips.index') }}"
-            class="text-gray-700 hover:bg-gray-200">旅のしおり</a>
-        <a href="{{ route('dashboard.trips.show', ['trip_id' =>$tripId ]) }}"
-            class="text-gray-700 hover:bg-gray-200">旅程表</a>
-        <a href="{{ route('dashboard.trips.memos.index', ['trip_id' =>$tripId ]) }}"
-            class="text-gray-700 hover:bg-gray-200">メモ一覧</a>
+<nav class="fixed w-full bottom-0 border-t border-gray-300 bg-white z-50">
+    <ul class="flex justify-around items-center text-center h-[64px]">
+        <li class="w-full hover:bg-sky-200 transition-all duration-200">
+            <a href="{{ route('dashboard.trips.index') }}"
+                class="flex flex-col justify-center items-center w-full h-full py-1">
+                <span>🗺</span>
+                <span class="mt-1 text-sm">旅のしおり</span>
+            </a>
+        </li>
+        <li class="w-full hover:bg-sky-200 transition-all duration-200">
+            <a href="{{ route('dashboard.trips.show', ['trip_id' => $tripId]) }}"
+                class="flex flex-col justify-center items-center w-full h-full py-1">
+                <span>📅</span>
+                <span class="mt-1 text-sm">旅程表</span>
+            </a>
+        </li>
+        <li class="w-full hover:bg-sky-200 transition-all duration-200">
+            <a href="{{ route('dashboard.trips.memos.index', ['trip_id' => $tripId]) }}"
+                class="flex flex-col justify-center items-center w-full h-full py-1">
+                <span>📝</span>
+                <span class="mt-1 text-sm">メモ一覧</span>
+            </a>
+        </li>
 
-        <!-- ➕ボタンのルート切り替え -->
-        @if ($currentRoute === 'dashboard.trips.show' || $currentRoute === 'dashboard.trips.itineraries.create')
-        <a href="{{ route('dashboard.trips.itineraries.create', ['trip_id' => $tripId]) }}"
-            class="text-gray-700 hover:bg-gray-200">➕</a>
-        @elseif ($currentRoute === 'dashboard.trips.memos.index' || $currentRoute === 'dashboard.trips.memos.create')
-        <a href="{{ route('dashboard.trips.memos.create', ['trip_id' => $tripId]) }}"
-            class="text-gray-700 hover:bg-gray-200">➕</a>
-        @endif
-    </div>
-</div>
+        <!-- ➕ボタンはルート切り替え -->
+        <li class="w-full hover:bg-sky-200 transition-all duration-200">
+            @if ($currentRoute === 'dashboard.trips.show' || $currentRoute === 'dashboard.trips.itineraries.create')
+            <a href="{{ route('dashboard.trips.itineraries.create', ['trip_id' => $tripId]) }}"
+                class="flex flex-col justify-center items-center w-full h-full py-1">
+                <span>➕</span>
+                <span class="mt-1 text-sm">新規作成</span>
+            </a>
+            @elseif ($currentRoute === 'dashboard.trips.memos.index' || $currentRoute === 'dashboard.trips.memos.create')
+            <a href="{{ route('dashboard.trips.memos.create', ['trip_id' => $tripId]) }}"
+                class="flex flex-col justify-center items-center w-full h-full py-1">
+                <span>➕</span>
+                <span class="mt-1 text-sm">新規作成</span>
+            </a>
+            @endif
+        </li>
+    </ul>
+</ｎ>
 @endif
